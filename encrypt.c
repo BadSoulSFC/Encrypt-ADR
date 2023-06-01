9e710e66734e6d32ab2806fd4bf94b2b1e48b9e2
BadSoulSFC <entropyzero93@gmail.com>
2023-06-01 21:03:41 +0400

    Added Base64 encoding&decoding

--- encrypt.c
+++ encrypt.c
@@ -1,4 +1,7 @@
 #include "argparse.h"
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +18,42 @@ void handleOpenSSLErrors()
 {
     ERR_print_errors_fp(stderr);
     exit(1);
+}
+
+// Base64 encoding
+char* base64_encode(const unsigned char* input, size_t length) {
+    BIO* b64_bio = BIO_new(BIO_f_base64());
+    BIO* mem_bio = BIO_new(BIO_s_mem());
+    BIO_push(b64_bio, mem_bio);
+    BIO_set_flags(b64_bio, BIO_FLAGS_BASE64_NO_NL);
+
+    BIO_write(b64_bio, input, length);
+    BIO_flush(b64_bio);
+
+    BUF_MEM* mem_buf;
+    BIO_get_mem_ptr(b64_bio, &mem_buf);
+
+    char* encoded_data = (char*)malloc(mem_buf->length);
+    memcpy(encoded_data, mem_buf->data, mem_buf->length - 1);
+    encoded_data[mem_buf->length - 1] = '\0';
+
+    BIO_free_all(b64_bio);
+
+    return encoded_data;
+}
+
+// Base64 decoding
+unsigned char* base64_decode(const char* input, size_t length) {
+    BIO* b64_bio = BIO_new(BIO_f_base64());
+    BIO* mem_bio = BIO_new_mem_buf(input, length);
+    BIO_push(b64_bio, mem_bio);
+
+    unsigned char* decoded_data = (unsigned char*)malloc(length);
+    BIO_read(b64_bio, decoded_data, length);
+
+    BIO_free_all(b64_bio);
+
+    return decoded_data;
 }
 
 void encryptFileAES(const char* inputFile, const char* outputFile, const unsigned char* key) {
@@ -46,6 +85,44 @@ void encryptFileAES(const char* inputFile, const char* outputFile, const unsigne
     fclose(outFile);
 }
 
+void encryptFileAESBASE64(const char* inputFile, const char* outputFile, const unsigned char* key) {
+    FILE *inFile, *outFile;
+    unsigned char inBuffer[AES_BLOCK_SIZE];
+    unsigned char outBuffer[AES_BLOCK_SIZE];
+    AES_KEY aesKey;
+    int bytesRead, padding;
+
+    inFile = fopen(inputFile, "rb");
+    outFile = fopen(outputFile, "wb");
+    if (inFile == NULL || outFile == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+
+    AES_set_encrypt_key(key, 128, &aesKey);
+
+    while ((bytesRead = fread(inBuffer, 1, AES_BLOCK_SIZE, inFile)) > 0) {
+        if (bytesRead < AES_BLOCK_SIZE) {
+            padding = AES_BLOCK_SIZE - bytesRead;
+            memset(inBuffer + bytesRead, padding, padding);
+        }
+        AES_encrypt(inBuffer, outBuffer, &aesKey);
+
+        // Converting the encrypted data to Base64
+        char* encoded_data = base64_encode(outBuffer, AES_BLOCK_SIZE);
+
+        // Writing the encoded data to the output file
+        fwrite(encoded_data, 1, strlen(encoded_data), outFile);
+
+        // Clean up
+        free(encoded_data);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+}
+
+
 void decryptFileAES(const char* inputFile, const char* outputFile, const unsigned char* key) {
     FILE *inFile, *outFile;
     unsigned char inBuffer[AES_BLOCK_SIZE];
@@ -75,6 +152,44 @@ void decryptFileAES(const char* inputFile, const char* outputFile, const unsigne
     fclose(inFile);
     fclose(outFile);
 }
+
+void decryptFileAESBASE64(const char* inputFile, const char* outputFile, const unsigned char* key) {
+    FILE *inFile, *outFile;
+    char inBuffer[AES_BLOCK_SIZE];
+    unsigned char outBuffer[AES_BLOCK_SIZE];
+    AES_KEY aesKey;
+    int bytesRead, padding;
+
+    inFile = fopen(inputFile, "rb");
+    outFile = fopen(outputFile, "wb");
+    if (inFile == NULL || outFile == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+
+    AES_set_decrypt_key(key, 128, &aesKey);
+
+    while ((bytesRead = fread(inBuffer, 1, AES_BLOCK_SIZE, inFile)) > 0) {
+        // Decoding the Base64 data
+        size_t decoded_length;
+        unsigned char* decoded_data = base64_decode(inBuffer, bytesRead, &decoded_length);
+
+        AES_decrypt(decoded_data, outBuffer, &aesKey);
+        if (bytesRead < AES_BLOCK_SIZE) {
+            padding = outBuffer[AES_BLOCK_SIZE - 1];
+            fwrite(outBuffer, 1, AES_BLOCK_SIZE - padding, outFile);
+        } else {
+            fwrite(outBuffer, 1, AES_BLOCK_SIZE, outFile);
+        }
+
+        // Clean up
+        free(decoded_data);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+}
+
 
 void encryptFileDES(const char* inputFile, const char* outputFile, const unsigned char* key) {
     FILE *inFile, *outFile;
@@ -193,14 +308,14 @@ int main(int argc, const char** argv)
         OPT_STRING('i', "input", &inputFile, "Input file"),
         OPT_STRING('o', "output", &outputFile, "Output file"),
         OPT_STRING('k', "key", &key, "Encryption key"),
-        OPT_STRING('a', "algorithm", &algorithm, "Encryption algorithm (aes, des, rsa)"),
+        OPT_STRING('a', "algorithm", &algorithm, "Encryption algorithm (aes, aes&base64, des, rsa)"),
         OPT_BOOLEAN('d', "decrypt", &decryptFlag, "Perform decryption"),
         OPT_END()
     };
 
     struct argparse argparse;
     argparse_init(&argparse, options, NULL, 0);
-    argparse_describe(&argparse, "\nFile Encryption Program", "\nEncrypt and decrypt files using AES, DES, or RSA algorithms.");
+    argparse_describe(&argparse, "\nFile Encryption Program", "\nEncrypt and decrypt files using AES, AES&BASE64, DES, or RSA algorithms.");
 
     argc = argparse_parse(&argparse, argc, argv);
 
